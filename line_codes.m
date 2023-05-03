f4e16e5c3140c0bdaa7e6580649f6c973c28933f
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2023-05-03 23:59:44 +0200

    Update line_codes.m

--- line_codes.m
+++ line_codes.m
@@ -2,7 +2,7 @@ clear all;
 clc;
 
 seq = randi([0 1], 1, 10);
-
+seq
 Tb = 1; % bit duration
 Fs = 100; % sampling frequency in Hz
 
@@ -19,7 +19,6 @@ for i = 1:length(seq)
     end
 end
 
-% Plot the modulated signal
 subplot(6, 1, 1);
 plot(t, y);
 axis([0 t(end) -2.5 2.5]);
@@ -39,8 +38,6 @@ for i = 1:length(seq)
   end
 end
 
-
-% Plot the modulated signal
 subplot(6, 1, 2);
 plot(t, y);
 axis([0 t(end) -2.5 2.5]);
@@ -60,8 +57,6 @@ for i = 1:length(seq)
   end
 end
 
-
-% Plot the modulated signal
 subplot(6, 1, 3);
 plot(t, y);
 axis([0 t(end) -2.5 2.5]);
@@ -70,3 +65,46 @@ ylabel('Amplitude');
 title('Manschester coding Modulation');
 %---------- Manschester coding ------------%
 
+%----------------- AMI ---------------%
+last_bit = 1; % will be used to check if the last 1-bit represented as 1 or -1
+for i = 1:length(seq)
+    if seq(i) == 0
+        y((i-1)*Fs+1:i*Fs) = 0;
+    else
+        y((i-1)*Fs+1:i*Fs) = last_bit;
+        last_bit = -1 * last_bit;
+    end
+end
+
+subplot(6, 1, 4);
+plot(t, y);
+axis([0 t(end) -2.5 2.5]);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('AMI Modulation');
+%----------------- AMI ---------------%
+
+%----------------- 3-level transmission ---------------%
+last_bit = 0;
+alter = [1 0 -1];
+j = 1;
+for i = 1:length(seq)
+    if seq(i) == 0
+        y((i-1)*Fs+1:i*Fs) = last_bit;
+    else
+         y((i-1)*Fs+1:i*Fs) = alter(j);
+         last_bit = alter(j);
+         j = j + 1;
+         if j > length(alter)
+             j = 1;
+         end
+    end
+end
+
+% Plot the modulated signal
+subplot(6, 1, 5);
+plot(t, y);
+axis([0 t(end) -4 2]);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('3-Level Transmission Modulation');
