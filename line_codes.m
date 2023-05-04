17f272b7eebbb0d3048494f430b4ec7558fb9a3c
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2023-05-04 12:42:53 +0200

    Update line_codes.m

--- line_codes.m
+++ line_codes.m
@@ -3,8 +3,11 @@ clc;
 
 seq = randi([0 1], 1, 10);
 
-Tb = 1; % bit duration
-Fs = 100; % sampling frequency in Hz
+Rb = 1; % bit rate
+Tb = 1/Rb; % bit duration
+Fs = 500; % sampling frequency in Hz
+f = 0:0.01:2*Rb; % frequency range
+a = 1; % PSD amplitude
 
 t = linspace(0, Tb*length(seq), length(seq)*Fs+1);
 t = t(1:end-1); % remove the last point to make sure that t and y have
@@ -27,7 +30,7 @@ ylabel('Amplitude');
 title('Polar NRZ Modulation');
 %----------------- polar NRZ ---------------%
 
-%----------------- polar RZ ---------------%
+%----------------- polar RZ ----------------%
 for i = 1:length(seq)
   if seq(i) == 1
     y((i-1)*Fs+1:(i-0.5)*Fs) = 1;
@@ -86,7 +89,7 @@ title('AMI Modulation');
 
 %------- 3-level transmission --------%
 last_bit = 0;
-alter = [1 0 -1];
+alter = [1 0 -1 0];
 j = 1;
 for i = 1:length(seq)
     if seq(i) == 0
@@ -109,8 +112,38 @@ ylabel('Amplitude');
 title('3-Level Transmission Modulation');
 %------- 3-level transmission --------%
 
+%--------------- NRZ inverted --------------%
+
+%--------------- NRZ inverted --------------%
 
 % Add super-title
 seq_str = num2str(seq);
 main_title = sprintf('Sequence: %s', seq_str);
 sgtitle(main_title);
+
+%----------------------------------
+%           PSD plots
+%----------------------------------
+arg = f * Tb;
+P_NRZ = (a^2) * Tb * sinc(arg) .* sinc(arg); 
+P_RZ = (a^2 / 2) * ((sinc(arg/2)).* (sinc(arg/2)));
+P_MAN = a^2 * Tb * (sinc(arg/2)).^2 .* (sin(pi*arg/2)).^2;
+P_AMI = (a^2 / (2*Tb)) * (sinc(2 * pi * arg / Tb)).^2;
+P_3LT = (a^2/Tb) * (0.5 * ((sin(pi*arg)./(pi*arg)).^2) + ...
+    ((1./(pi*arg)).^2) .* ((sin(2*pi*arg)./2).^2));
+    
+figure(2)
+hold on
+plot(f,P_NRZ,'r')
+plot(f,P_RZ,'g')
+plot(f,P_MAN,'b')
+plot(f,P_AMI,'k')
+plot(f, P_3LT, 'm')
+grid on
+box on
+xlabel('Frequency')
+ylabel('Power Spectral Density')
+title('PSD for the Line Codes')
+legend('PSD for Polar NRZ Signal','PSD for polar RZ Signal',...
+    'PSD for Manchester Signal', 'PSD for AMI',...
+    'PSD for 3-level transmission');
